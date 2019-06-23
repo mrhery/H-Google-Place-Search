c5a28e0557b8455f38ad6fe64be2be004a6ac93e
Mr Hery <aimanmrc@gmail.com>
2019-06-23 10:03:57 +0800

    Create rpc.php

---
+++ rpc.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$url = Input::post("url") . "?";
+
+$acc = ["key", "location", "address", "type", "radius", "placeid", "fields", "maxwidth", "maxheight", "photoreference"];
+
+foreach($_POST as $key => $value){
+  if(in_array($key, $acc)){
+    if($key != "url"){
+      $url .= $key . "=" . urlencode($value) . "&";
+    }
+  }
+}
+
+try{
+  $c = new Curl($url);
+
+  echo $c->getResponse();
+}catch(Exception $e){
+  echo json_encode([
+    "status"	=> "error",
+    "message"	=> "Fail executing request",
+    "data"		=> $e
+  ]);
+}
